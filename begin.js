97bd58fff93223f9823b8d6b39a93a8e1118277b
KS-Abhi <93191466+KS-Abhi@users.noreply.github.com>
2021-11-28 23:36:43 +0530

    Changes
    index.html-bg color added
    begin.js- list of items
    login.js- validation only

--- begin.js
+++ begin.js
@@ -1,27 +1,73 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function(){
      
-    if(this.readyState==4&&this.status==200){
-        var response = JSON.parse(this.responseText);
-        var items = "<table id='itemTbl'>";
+//     if(this.readyState==4&&this.status==200){
+//         var response = JSON.parse(this.responseText);
+//         var items = "<table id='itemTbl'>";
 
-        for(let i=0; i<response.length;i++){
-            items=items+`<tr><td>`
-            if(response[i].completed===true){
-                items=items+`<input type='checkbox'id='chkBx${i}'></td>
-                <td><label id='lbl${i}'>${response[i].title}</label></td>`;
-            } 
-            else{
-                items=items+`<input type='checkbox' id='chkBx${i}' onclick='chkItem(this)'></td>
-                <td><label id='lbl${i}''>${response[i].title}</label></td>`
-            }  
-            items=items+`</td></tr>`;    
-         }
-         items=items+`</table>`
+//         for(let i=0; i<response.length;i++){
+//             items=items+`<tr><td>`
+//             if(response[i].completed===true){
+//                 items=items+`<input type='checkbox'id='chkBx${i}'></td>
+//                 <td><label id='lbl${i}'>${response[i].title}</label></td>`;
+//             } 
+//             else{
+//                 items=items+`<input type='checkbox' id='chkBx${i}' onclick='chkItem(this)'></td>
+//                 <td><label id='lbl${i}''>${response[i].title}</label></td>`
+//             }  
+//             items=items+`</td></tr>`;    
+//          }
+//          items=items+`</table>`
 
-         document.getElementById("itemList").innerHTML=items;
+//          document.getElementById("itemList").innerHTML=items;
+//     }
+// }
+
+// xhttp.open('GET','https://jsonplaceholder.typicode.com/todos', true);
+// xhttp.send();
+
+function todoList() {
+    var xhttp = new XMLHttpRequest();
+  
+    xhttp.onreadystatechange = function () {
+  
+      if (this.readyState == 4 && this.status == 200) {
+  
+        var response = JSON.parse(this.responseText)
+  
+        for (i = 0; i <= response.length; i++) {
+  
+          var data =
+            `<li class="list-group-item d-flex"  style="overflow-y:auto">
+                        <div class="d-flex align-items-center">
+                          <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." id="cheq" onclick="countCheckBox()"/>
+                          ${response[i].id}
+                          ${response[i].title}
+                        </div>
+              </li>`
+          document.getElementById("itemList").innerHTML += data;
+        }
+      }
     }
-}
-
-xhttp.open('GET','https://jsonplaceholder.typicode.com/todos', true);
-xhttp.send();
+  
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true)
+    xhttp.send();
+  }
+  
+  
+  function countCheckBox() {
+  
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  
+    if (checkboxes.length == 5) {
+      alert(' Congrats. 5 Tasks have been Successfully Completed');
+    }
+  
+  }
+  
+  
+  function logout(){
+    window.location = "login.html";
+  }
+  
+  window.addEventListener("load", todoList);
--- login.js
+++ login.js
@@ -1,53 +1,19 @@
 function validate(){
-    let userName = document.getElementById("userName");
+    let userName = document.getElementById("usrName");
     let password = document.getElementById("password");
-    let error = document.getElementById("error");
-    let error1 = document.getElementById("error1");
+    let usrError = document.getElementById("usrError");
+    let pwdError = document.getElementById("pwdError");
     
     if(userName.value == "" || password.value == ""){
         if(userName.value == ""){
-            error.innerText = "Enter Username";
-            error.style.color = "red";
+            usrError.innerText = "Enter Username";
+            usrError.style.color = "red";
         }
 
         if(password.value == "") {
-            error1.innerText = "Enter Password";
-            error1.style.color = "red";
+            pwdError.innerText = "Enter Password";
+            pwdError.style.color = "red";
         }
             return false;
     }
-}
-// User authentication using callback
-function loginUsr(user,pwd,callBack){
-    const strUser="admin";
-    let admin,password;
-    if(user===strUser){
-        admin=true;
-    }
-    else{
-        document.getElementById("error").innertext="Username incorrect";
-        admin=false;
-    }
-    password=callBack(pwd);
-    if(admin&&password)
-    return true;
-    else
-    return false;
-}
-// Password authentication using callback
-function usrPwd(pwd){
-      const strPwd=12345;
-      if(pwd==strPwd){
-        return true;
-      }
-   else{
-    document.getElementById("error1").innerText="Incorrect password";
-    return false;
-   }
-}
-function clrUser(){
-    document.getElementById("error").innerText="";
-}
-function clrPwd(){
-    document.getElementById("error1").innerText="";
 }
