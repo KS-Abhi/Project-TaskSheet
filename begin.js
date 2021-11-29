35e9cbabcbbab9f92f2fbe90cf2b43481445a90b
KS-Abhi <93191466+KS-Abhi@users.noreply.github.com>
2021-11-29 10:23:36 +0530

    Change

--- begin.js
+++ begin.js
@@ -1,37 +1,9 @@
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function(){
-     
-//     if(this.readyState==4&&this.status==200){
-//         var response = JSON.parse(this.responseText);
-//         var items = "<table id='itemTbl'>";
-
-//         for(let i=0; i<response.length;i++){
-//             items=items+`<tr><td>`
-//             if(response[i].completed===true){
-//                 items=items+`<input type='checkbox'id='chkBx${i}'></td>
-//                 <td><label id='lbl${i}'>${response[i].title}</label></td>`;
-//             } 
-//             else{
-//                 items=items+`<input type='checkbox' id='chkBx${i}' onclick='chkItem(this)'></td>
-//                 <td><label id='lbl${i}''>${response[i].title}</label></td>`
-//             }  
-//             items=items+`</td></tr>`;    
-//          }
-//          items=items+`</table>`
-
-//          document.getElementById("itemList").innerHTML=items;
-//     }
-// }
-
-// xhttp.open('GET','https://jsonplaceholder.typicode.com/todos', true);
-// xhttp.send();
-
 function todoList() {
     var xhttp = new XMLHttpRequest();
   
-    xhttp.onreadystatechange = function () {
+    xhttp.onreadystatechange = function(){
   
-      if (this.readyState == 4 && this.status == 200) {
+      if (this.readyState==4&&this.status==200){
   
         var response = JSON.parse(this.responseText)
   
@@ -59,7 +31,7 @@ function todoList() {
   
     var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
   
-    if (checkboxes.length == 5) {
+    if (checkboxes.length === 5) {
       alert(' Congrats. 5 Tasks have been Successfully Completed');
     }
   
