94d2cf32b3fa39d82535e804a942298aaedd3b57
KS-Abhi <93191466+KS-Abhi@users.noreply.github.com>
2021-11-29 14:01:53 +0530

    Change
    login.js- Authentication added

--- login.js
+++ login.js
@@ -17,3 +17,43 @@ function validate(){
             return false;
     }
 }
+//call back user authentication
+function testUser(user,pwd,callBack){
+const strUser="admin";
+let admin,password;
+  if(user.toLowerCase()==strUser){
+  admin=true;
+  }
+  else{
+  document.getElementById("usrError").innerText="Invalid UserName";
+  admin=false;
+  }
+  password=callBack(pwd);
+  if(admin&&password)
+  return true;
+  else
+  return false;
+}
+
+
+//call backfunction test password n authenticate login
+function testPwd(pwd){
+    const strPwd=12345;
+    if(pwd==strPwd){
+        return true;
+    }
+
+    else{
+    document.getElementById("pwdError").innerText="Invalid Password";
+    return false;
+    }
+} 
+
+function clearUser(){
+    document.getElementById("usrError").innerText="";
+}
+
+function clearPwd(){
+    document.getElementById("pwdError").innerText="";
+}
+
